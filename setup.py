a8604aeeae98d3b593f2d3a989a6fc7b3067bd74
Ouwen <leeowen988@gmail.com>
2018-12-19 14:13:46 +0200

    init setup.py

---
+++ setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name='ruler',
+    version='1.0',
+    packages=[''],
+    url='',
+    license='',
+    author='Ouwen Li',
+    author_email='leeowen988@gmail.com',
+    description='Ordinary differential equation simulation for plastic bending physics experiment',
+    py_modules=[],
+    install_requires=["numpy"],
+    package_data=[]
+)
