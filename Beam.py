7c9e07306cdd19cf2fce525fa4c3205ef0ddc1d2
Ouwen <leeowen988@gmail.com>
2019-01-22 13:00:45 +0200

    update cantilever beam matrix

--- Beam.py
+++ Beam.py
@@ -2,14 +2,14 @@
 
 import numpy, matplotlib.pyplot
 
-kb=600.
-ks=100.
+kb=0.5 #usualy the bending stiffness of PP (plastic) ranges from 1 to 10 GPa. 1 GPa=1000N/mm^2=0.1N/cm^2
+ks=1.4
 steps=100
 h=1./steps
 a0=6*kb+2*ks*h**2
 a1=-4*kb-ks*h**2
 a2=kb
-length=40. #unit:cm
+length=30. #unit:cm
 
 
 def Fixed_End_Beam(delta):
@@ -56,7 +56,7 @@ def Fixed_End_Beam(delta):
     return x
 
 
-def Cantilever_Beam(delta):
+def Cantilever_Beam(delta,f):
     A=numpy.zeros((steps+1,steps+1))
     b=numpy.zeros(steps+1)
 
@@ -68,7 +68,7 @@ def Cantilever_Beam(delta):
     A[1][2]=a1
     A[1][3]=a2
 
-    for i in range(2,steps-1):
+    for i in range(2,steps-2):
         A[i][i-2]=a2
         A[i][i-1]=a1
         A[i][i]=a0
@@ -76,11 +76,17 @@ def Cantilever_Beam(delta):
         A[i][i+2]=a2
         b[i]=0
 
-    A[steps - 1][steps - 3] = a2
-    A[steps - 1][steps - 2] = a1
-    A[steps - 1][steps - 1] = a0 - a2
-    A[steps-1][steps]=a1+2*a2
-    b[steps-1]=0
+    A[steps - 2][steps - 4] = a2
+    A[steps - 2][steps - 3] = a1
+    A[steps - 2][steps - 2] = a0 - a2
+    A[steps-2][steps-1]=a1+2*a2
+    b[steps-2]=0
+
+    A[steps-1][steps-3]=-1.
+    A[steps-1][steps-2]=3.
+    A[steps-1][steps-1]=-3.
+    A[steps-1][steps]=1
+    b[steps-1]=h**3*f/kb
 
     A[steps][steps]=1.
     b[steps]=delta
@@ -191,17 +197,24 @@ def plot_cantilever_beam(x, y):
 
 if __name__ == "__main__":
     x=numpy.zeros(steps+1)
+
+    delta = -1.06
     for i in range(steps+1):
         x[i]=length*i/steps
-
-    delta = -3.
     y=Simply_Supported_Beam(delta)
     plot_simply_supported_beam(x,y)
 
-    delta = -3.
+    delta = -1.5
+    length=40.8
+    for i in range(steps+1):
+        x[i]=length*i/steps
     y = Fixed_End_Beam(delta)
     plot_fixed_end_beam(x,y)
 
-    delta = -3.
-    y = Cantilever_Beam(delta)
+    delta = -7.2
+    length=39
+    f=40./1000.*9.8
+    for i in range(steps+1):
+        x[i]=length*i/steps
+    y = Cantilever_Beam(delta,f)
     plot_cantilever_beam(x,y)
